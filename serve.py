66502af7659da1a1c10adb1837c91d694b787fae
Transactional Blog <transactional.blog@gmail.com>
2024-05-27 23:45:55 -0700

    serve.py notices ruby changes, and builds first when run

--- serve.py
+++ serve.py
@@ -2,5 +2,10 @@
 import livereload
 
 server = livereload.Server()
-server.watch('source/', livereload.shell('bundle exec middleman build --environment=development'))
+middleman_build = livereload.shell('bundle exec middleman build --environment=development')
+middleman_build()
+server.watch('source/', middleman_build)
+server.watch('config.rb', middleman_build)
+server.watch('asciidoc_extensions', middleman_build)
+server.watch('asciidoc_templates', middleman_build)
 server.serve(root='build/', port=4567)
