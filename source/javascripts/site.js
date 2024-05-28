2a30a377a2e6ea46c5de508093a2920a3f57fd94
Transactional Blog <transactional.blog@gmail.com>
2024-05-27 23:45:54 -0700

    Add a pie chart to TLS survey, which was shocking difficult.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,17 @@
-// This is where it all goes :)
+const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
+function tableToData(tableId) {
+    var tblhdr = $(`table#${tableId} tr`).get().map(function(row) {
+    return $(row).find('th').get().map(function(cell) {
+        return $(cell).text();
+    }).slice(0,3);
+    })[0];
+
+    var tbldata = $(`table#${tableId} tr`).get().map(function(row) {
+    return Object.fromEntries(zip(tblhdr, $(row).find('td').get().map(function(cell) {
+        return $(cell).text();
+    }).slice(0,3)));
+    }).slice(1);
+
+    return tbldata;
+}
