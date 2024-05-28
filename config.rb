f031ae04ba6a562e983b090b4f9d61fb13242d25
Transactional Blog <transactional.blog@gmail.com>
2024-05-27 23:45:53 -0700

    Ignore vim temporary files

--- config.rb
+++ config.rb
@@ -37,6 +37,8 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+ignore '.*.swp'
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
