917c7eb83e8b1f7b1a6f3bc8e00fe0a68dce1c4b
Transactional Blog <transactional.blog@gmail.com>
2024-05-27 23:45:56 -0700

    Overhaul CSS to remove grid and improve sidenotes

---
+++ source/javascripts/aside-positioning.js
@@ -0,0 +1,38 @@
+function positionAsideElements(mq) {
+    if (!mq.matches) {
+        return;
+    }
+
+    // Find all 'aside' elements
+    const asideElements = Array.from(document.querySelectorAll('aside,.aside,.toc'));
+
+    asideElements.sort( (lhs,rhs) => lhs.getBoundingClientRect().top - rhs.getBoundingClientRect().top);
+
+    main_element = document.querySelector('main');
+    if (main_element === null) {
+        return;
+    }
+    baseline = main_element.getBoundingClientRect().top;
+
+    // Iterate through the 'aside' elements from top down
+    asideElements.forEach((aside, index) => {
+        // Adjust the top position of each aside below the first one
+        if (aside.className == 'postmeta') {
+            aside.style.top = document.querySelector('h1').getBoundingClientRect().top - baseline + 'px';
+        }
+        if (index > 0) {
+            const previousAside = asideElements[index - 1];
+            const previousAsideBottom = previousAside.getBoundingClientRect().bottom;
+            const myTop = aside.getBoundingClientRect().top;
+            console.log("myTop=" + myTop + " prevBot=" + previousAsideBottom)
+            if (previousAsideBottom > myTop) {
+                aside.style.top = previousAsideBottom - baseline + 'px';
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  positionAsideElements({matches: true});
+});
+window.matchMedia("(min-width: 910px)").addEventListener('change', positionAsideElements)
