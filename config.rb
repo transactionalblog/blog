0c747acf621b80fa24a1f00cdca2babae58d7ebb
Transactional Blog <transactional.blog@gmail.com>
2024-06-15 23:02:18 -0700

    Also don't rewrite bamboo css

--- config.rb
+++ config.rb
@@ -14,7 +14,7 @@ require_relative 'asciidoc_extensions/postprocessors'
 activate :asciidoc, backend: 'xhtml5', safe: :unsafe, template_dirs: 'asciidoc_templates', attributes: ['source-highlighter=rouge', 'toc-title=']
 set :skip_build_clean, proc {|f| f.start_with? 'build/images/'}
 
-activate :asset_hash, :ignore => %r{^images/rss.svg}
+activate :asset_hash, :ignore => [%r{^images/rss.svg}, %r{^stylesheets/bamboo/.*.css}]
 
 class ImageDirPerAsciidoc < Middleman::Extension
   def initialize(app, options_hash={}, &block)
